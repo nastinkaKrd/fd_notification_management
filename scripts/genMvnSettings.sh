82393c09fa06f24e5e8e6327727fc82f1679e108
nastinkaKrd <nastinka352@gmail.com>
2025-03-13 16:03:46 +0200

    try coveralls

--- scripts/genMvnSettings.sh
+++ scripts/genMvnSettings.sh
@@ -1,10 +1,11 @@
+mkdir $HOME/.m2/
 cat <<EOF | tee $HOME/.m2/settings.xml
 <settings>
   <servers>
     <server>
-      <id>my-external-repo</id>
-      <username>${REPO_USERNAME}</username>
-      <password>${REPO_PASSWORD}</password>
+      <id>github</id>
+      <username>${GITHUB_USERNAME}</username>
+      <password>${GH_PACKAGES_TOKEN}</password>
     </server>
   </servers>
 </settings>
